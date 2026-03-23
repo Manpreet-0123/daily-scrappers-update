4f1641299dcd85bcd7347e22df3927f951119108
Manpreet <manpreet.s@getnitro.co>
2026-03-23 10:45:58 +0530

    updation_date

--- src/components/data_extraction.py
+++ src/components/data_extraction.py
@@ -13,13 +13,11 @@ class DataExtractionPath:
 
 class DataExtraction:
     def save_dataframe_to_csv(self):
-            # end_date = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
-            # start_date = end_date - timedelta(days=1)
-            end_date = '2026-03-20 00:00:00.00'
-            start_date = '2026-03-01 00:00:00.00'
+            end_date = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
+            start_date = end_date - timedelta(days=1)
 
-            # start_date_str = start_date.strftime('%Y-%m-%d %H:%M:%S.00')
-            # end_date_str = end_date.strftime('%Y-%m-%d %H:%M:%S.00')
+            start_date_str = start_date.strftime('%Y-%m-%d %H:%M:%S.00')
+            end_date_str = end_date.strftime('%Y-%m-%d %H:%M:%S.00')
 
             open_bastion_connection()
             os.makedirs(DataExtractionPath.data_extraction_path, exist_ok=True)
@@ -37,7 +35,7 @@ class DataExtraction:
                 
                 with pd.ExcelWriter(excel_file_path, engine='openpyxl', mode='a' if os.path.exists(excel_file_path) else 'w') as writer:
                     for channel in channels:
-                        sql_query = scrappers_query(start_date, end_date, channel)
+                        sql_query = scrappers_query(start_date_str, end_date_str, channel)
                         success, df, message = execute_query_to_dataframe(sql_query)
 
                         if success:
